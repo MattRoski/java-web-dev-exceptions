8f94e63f371fb7c3309eff7788e344f4097bb35b
MattRoski <mattroski88@gmail.com>
2022-05-16 14:23:55 -0500

    Finished exercise 11

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -10,23 +10,49 @@ public class Main {
         // Test out your Divide() function!
 
         HashMap<String, String> studentFiles = new HashMap<>();
-        studentFiles.put("Carl", "Program.java");
+        studentFiles.put("Carl", "Program.jva");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for(Map.Entry<String,String> set: studentFiles.entrySet()){
+            System.out.println(set.getKey() + " " +CheckFileExtension(set.getValue() ));
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static double Divide(int x, int y)
     {
         // Write code here!
+        if(y==0) {
+            try  {
+                throw new ArithmeticException("You cannot divide by zero!");
+            } catch(ArithmeticException e){
+                e.printStackTrace();
+            }
+        }
+        return (1.0*x)/y;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+    public static int CheckFileExtension(String fileName) {
         // Write code here!
-        return -1;
+        // if file ends in .java +1
+        //if file doesnt end in .java 0
+        //if file is null or emptystring exception thrown
+        int output = 0;
+        if (fileName == null || fileName == "") {
+            output = -1;
+
+            throw new RuntimeException("fileName cannot be null or empty");
+
+        }
+        if (!fileName.contains(".java")) {
+            output =  0;
+        } else if (fileName.contains(".java")) {
+            output =  1;
+        }
+
+        return output;
     }
 
 }
